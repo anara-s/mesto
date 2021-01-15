bad63a3b61cce6561e8842f996cb0b0859ba2458
Anara S <anara.sl@ya.ru>
2021-01-15 20:20:23 +0300

    Проектная работа 4

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let openButton = document.querySelector('.profile__edit');
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close');
+
+let formElement = popup.querySelector('.popup__container')
+
+let nameInput = formElement.querySelector('.popup__author');
+let jobInput = formElement.querySelector('.popup__description');
+
+let author = document.querySelector('.profile__author');
+let description = document.querySelector('.profile__description');
+
+function togglePopup() {
+    popup.classList.toggle('popup_opened')
+}
+
+openButton.addEventListener('click', () => {
+    nameInput.value = author.textContent
+    jobInput.value = description.textContent
+    togglePopup ()
+});
+
+closeButton.addEventListener('click', togglePopup)
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();    
+
+    author.textContent = nameInput.value;
+    description.textContent = jobInput.value;
+
+    togglePopup()
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
